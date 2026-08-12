6f742a0550b1ac2b02002ded2334fe59f1539ab9
techy-amber <jainamber758@gmail.com>
2026-08-13 01:17:50 +0530

    Allow deployed frontend in CORS

--- src/api/app.py
+++ src/api/app.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://mcp-client-1-ew4q.onrender.com"
     ],
 
     allow_credentials=True,
